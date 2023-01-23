954d1d75b90619c373b54496a548695ce49791fb
Delfiniotis <panos_karageorgios@hotmail.com>
2023-01-23 20:55:26 +0000

    First commit

---
+++ little-lemon/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <text className="text" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
